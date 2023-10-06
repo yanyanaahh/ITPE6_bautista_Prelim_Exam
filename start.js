330774e503e4d37dfb66426e1a90343d1ca670f3
yanyanaahh <allyanahbautista@gmail.com>
2023-10-06 23:15:54 +0800

    added file start.js

---
+++ start.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const dotenv = require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
